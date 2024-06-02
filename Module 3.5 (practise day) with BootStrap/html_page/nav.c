097b69c9f78c8e6a892ee449095b9cc1b484d0d2
Md. Jubaer Mahmud Sarker <147659414+jmsman3@users.noreply.github.com>
2024-06-03 00:20:04 +0600

    Add files via upload

---
+++ Module 3.5 (practise day) with BootStrap/html_page/nav.c
@@ -0,0 +1,222 @@
+// <nav class="navbar navbar-expand-lg bg-body-tertiay container">
+//     <div class="container-fluid ">
+//       <a class="navbar-brand fw-bold">Hockey<span class="koma">'</span><span>s</span></a>
+//       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+//         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//         <span class="navbar-toggler-icon"></span>
+//       </button>
+//       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//         <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-5 ">
+//           <li class="nav-item">
+//             <a class="nav-link active" aria-current="page" href="#">Home</a>
+//           </li>
+//           <li class="nav-item">
+//             <a class="nav-link" href="#">About</a>
+//           </li>
+
+
+//           <li class="nav-item">
+//             <a class="nav-link" href="#">Pages</a>
+//           </li>
+
+//           <li class="nav-item">
+//             <a class="nav-link" href="#">Blog</a>
+//           </li>
+
+//           <li class="nav-item">
+//             <a class="nav-link" href="#">Contact</a>
+//           </li>
+//           <!-- <li class="nav-item dropdown">
+//             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//               Dropdown
+//             </a>
+//             <ul class="dropdown-menu">
+//               <li><a class="dropdown-item" href="#">Action</a></li>
+//               <li><a class="dropdown-item" href="#">Another action</a></li>
+//               <li><hr class="dropdown-divider"></li>
+//               <li><a class="dropdown-item" href="#">Something else here</a></li>
+//             </ul>
+//           </li>
+//           <li class="nav-item">
+//             <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+//           </li> -->
+//         </ul>
+//         <form class="d-flex" role="search">
+//           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+//           <button class="btn btn-outline-success" type="submit">Search</button>
+//         </form>
+//       </div>
+//     </div>
+//   </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     <section class="dual_card d-flex gap-5  justify-content-center" style=" align-content: center;">
+//       <div class="left">
+//         <div class="card" style="width: 40rem; margin-top:  70px;">
+//           <img src="/images/2.png" class="card-img-top" alt="...">
+//           <div class="card-body">
+//             <h3 class="card-title">Junior program</h3>
+//             <p class="card-text">Amader aikhane ase juniorder jonno traning program. akane junior baccha kaccha ra
+//               taraing nente parbe.Sobar jonno ase sundor tranning er babosta.apnader ashe pashe jodi hockey khelte
+//               pososdo kore amon polapan thake tahole tader invite korben.</p>
+//             <a href="#" class="btn btn-primary stretched-link">Register Now</a>
+//           </div>
+//         </div>
+//       </div>
+// <!-- ---------------------------------------------------------------------------------------------------------------------------------------------------------------- -->--
+
+//       <div class="right">
+
+//         <div class="card" style="width: 40rem; margin-top:  70px;">
+//           <img src="/images/4.png" class="card-img-top " style="height: 376px;" alt="...">
+//           <div class="card-body">
+//             <h3 class="card-title">Teenager Program</h3>
+//             <p class="card-text">Amader aikhane ase Teenager jonno traning program. akane tenn baccha kaccha ra taraing
+//               nente parbe.Sobar jonno ase sundor tranning er babosta.apnader ashe pashe jodi hockey khelte pososdo kore
+//               amon polapan thake tahole tader invite korben.</p>
+//             <a href="#" class="btn btn-primary stretched-link">Register Now </a>
+//           </div>
+//         </div>
+
+//       </div>
+//     </section>
+// <!-- ---------------------------------------------------------------------------------------------------------------------------------------------------------------- -->--
+
+//     <section>
+//       <div class="below_long_image d-flex justify-content-center">
+
+//         <div class="card" style="width: 83rem; margin-top: 40px;">
+//           <img src="/images/4.png" class="card-img-top" alt="...">
+//           <div class="card-body">
+//             <h3 class="card-title">Professional Program</h3>
+//             <p class="card-text">Amader aikhane ase Professional traning program. akane tenn baccha kaccha ra taraing
+//               nente parbe.Sobar jonno ase sundor tranning er babosta.apnader ashe pashe jodi hockey khelte pososdo kore
+//               amon polapan thake tahole tader invite korben.
+//               Amader aikhane ase Professional traning program. akane tenn baccha kaccha ra taraing nente parbe.Sobar
+//               jonno ase sundor tranning er babosta.apnader ashe pashe jodi hockey khelte pososdo kore amon polapan thake
+//               tahole tader invite korben.
+//             </p>
+//             <a href="#" class="btn btn-primary stretched-link">Register Now </a>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+
+
+
+// /* Progess nav Css */
+// .progress-nav {
+//     display: flex;
+//     justify-content: space-evenly;
+//     margin-top: 100px;
+//      /* width: 88%;
+//     border: 1px solid white;
+//     box-shadow: 5px 5px 10px gray;
+//     align-items: center;
+//     align-content: center;
+//     margin-left: 7%;
+//     border-radius: 22px;  */
+//   }
+
+//   .progress {
+//     width: 150px;
+//     height: 150px !important;
+//     line-height: 150px;
+//     background: none;
+//     margin: 20px;
+//     box-shadow: none;
+//     position: relative;
+//   }
+
+//   .progress:after {
+//     content: "";
+//     width: 100%;
+//     height: 100%;
+//     border-radius: 50%;
+//     border: 12px solid #fff;
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//   }
+
+//   .progress>span {
+//     width: 50%;
+//     height: 100%;
+//     overflow: hidden;
+//     position: absolute;
+//     top: 0;
+//     z-index: 1;
+//   }
+
+//   .progress .progress-left {
+//     left: 0;
+//   }
+
+//   .progress .progress-bar {
+//     width: 100%;
+//     height: 100%;
+//     background: none;
+//     border-width: 12px;
+//     border-style: solid;
+//     position: absolute;
+//     top: 0;
+//   }
+
+//   .progress .progress-left .progress-bar {
+//     left: 100%;
+//     border-top-right-radius: 80px;
+//     border-bottom-right-radius: 80px;
+//     border-left: 0;
+//     transform-origin: center left;
+//   }
+
+//   .progress .progress-right {
+//     right: 0;
+//   }
+
+//   .progress .progress-right .progress-bar {
+//     left: -100%;
+//     border-top-left-radius: 80px;
+//     border-bottom-left-radius: 80px;
+//     border-right: 0;
+//     transform-origin: center right;
+//   }
+
+//   .progress .progress-value {
+//     width: 90%;
+//     height: 90%;
+//     border-radius: 50%;
+//     background: #000;
+//     font-size: 24px;
+//     color: #fff;
+//     line-height: 135px;
+//     text-align: center;
+//     position: absolute;
+//     top: 5%;
+//     left: 5%;
+//   }
+
+
+
+
+
+
